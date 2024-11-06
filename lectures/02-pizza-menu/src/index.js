d2db9786a22bea7cfbfd4ca70301fa39f73e742b
guilhermerezende10 <rezendeguilherme733@gmail.com>
2024-11-06 17:20:12 -0300

    Conditional Rendering With Multiple Returns

--- lectures/02-pizza-menu/src/index.js
+++ lectures/02-pizza-menu/src/index.js
@@ -87,6 +87,8 @@ function Menu() {
 }
 
 function Pizza(props) {
+  if (props.pizzaObj.soldOut) return null;
+
   return (
     <li className="pizza">
       <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name}></img>

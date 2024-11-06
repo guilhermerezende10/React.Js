af271e5396dbe6eb0c76e6d2e798ad71a79410d7
guilhermerezende10 <rezendeguilherme733@gmail.com>
2024-11-06 17:24:32 -0300

    Extracting JSX Into a New Component

--- lectures/02-pizza-menu/src/index.js
+++ lectures/02-pizza-menu/src/index.js
@@ -107,25 +107,27 @@ function Footer() {
   const closeHour = 16;
   const isOpen = hour >= openHour && hour < closeHour;
 
-  //   alert(
-  //     hour >= openHour && hour <= closeHour
-  //       ? "We are currently open!"
-  //       : "Sorry we're closed"
-  //   );
-
   return (
     <footer className="footer">
       {isOpen ? (
-        <div className="order">
-          <p>We're opent unti {closeHour}:00. Come visit us or order online.</p>
-          <button className="btn">Order</button>
-        </div>
+        <Order closeHour={closeHour} />
       ) : (
         <p>
           We're happy to welcome you between {openHour}:00 and {closeHour}:00.
         </p>
       )}
     </footer>
+  );
+}
+
+function Order(props) {
+  return (
+    <div className="order">
+      <p>
+        We're open until {props.closeHour}:00. Come visit us or order online.
+      </p>
+      <button className="btn">Order</button>
+    </div>
   );
 }
 

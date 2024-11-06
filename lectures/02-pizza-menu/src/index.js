93cd13a91c1e54e73d2d7d3204b76d47992bab0a
guilhermerezende10 <rezendeguilherme733@gmail.com>
2024-11-06 18:00:08 -0300

    React Fragments

--- lectures/02-pizza-menu/src/index.js
+++ lectures/02-pizza-menu/src/index.js
@@ -74,11 +74,18 @@ function Menu() {
       <h2>Our menu</h2>
 
       {hasPizzas ? (
-        <ul className="pizzas">
-          {pizzaData.map((pizza) => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-          ))}
-        </ul>
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creatives dishes to choose from. All
+            from our stone oven, all organic, all delicious.
+          </p>
+
+          <ul className="pizzas">
+            {pizzaData.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
       ) : (
         <p>We're still working on our menu. Please come back later :)</p>
       )}

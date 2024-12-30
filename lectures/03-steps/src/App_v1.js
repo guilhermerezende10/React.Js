586dcacbd76a5d63abfee1364f66ae616ec47790
guilhermerezende10 <rezendeguilherme733@gmail.com>
2024-12-30 16:11:30 -0300

    The "children" Prop: Making a Reusable Button

---
+++ lectures/03-steps/src/App_v1.js
@@ -0,0 +1,63 @@
+import { useState } from "react";
+
+const messages = [
+  "Learn React!!",
+  "Apply for jobs!!",
+  "Invest your new income!!",
+];
+
+export default function App() {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
+  function handlePrevious() {
+    if (step > 1) setStep((curStep) => curStep - 1);
+  }
+  function handleNext() {
+    if (step < 3) setStep((curStep) => curStep + 1);
+  }
+
+  return (
+    <div>
+      <button className="close" onClick={() => setIsOpen((is) => !is)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
+          <div className="buttons">
+            <Button
+              bgColor="#7950f2"
+              textColor="#fff"
+              onClick={handlePrevious}
+              ><span>👈🏻</span>Previous </Button>
+            <Button
+              bgColor="#7950f2"
+              textColor="#fff"
+              onClick={handleNext}
+            >Next <span>👉🏻</span> </Button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+
+function Button({textColor, bgColor, onClick, text, children}) {
+  return (
+    <button
+      style={{ backgroundColor: bgColor, color: textColor }}
+      onClick={onClick}
+    >
+    {children}
+    </button>
+  );
+}

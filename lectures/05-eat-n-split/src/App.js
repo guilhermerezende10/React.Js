399c77e7ed3f92c849b43c8873f74b50acc2d462
guilhermerezende10 <rezendeguilherme733@gmail.com>
2024-12-31 10:32:21 -0300

    Displaying the New Friend Form

--- lectures/05-eat-n-split/src/App.js
+++ lectures/05-eat-n-split/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialFriends = [
   {
     id: 118836,
@@ -19,13 +21,21 @@ const initialFriends = [
   },
 ];
 
+function Button({children, onClick}) {
+  return <button className="button" onClick={onClick}>{children}</button>
+}
 export default function App() {
+  const [showAddFriend, setShowAddFriend] = useState(false);
+
+  function handleShowAddFriend() {
+    setShowAddFriend(show => !show)
+  }
   return (
     <div className="app">
       <div className="sidebar">
         <FriendsList />
-        <FormAddFriend />
-        <Button>Add friend </Button>
+        {showAddFriend && <FormAddFriend />}
+        <Button onClick={handleShowAddFriend}>{showAddFriend ? "Close" : "Add friend"} </Button>
       </div>
 
       <FormSplitBill />
@@ -65,16 +75,13 @@ function Friend({ friend }) {
     </li>
   );
 }
-function Button({children}) {
-  return <button className="button">{children}</button>
-}
 
 function FormAddFriend() {
   return <form className="form-add-friend">
-    <label>Friend name</label>
+    <label>👫Friend name</label>
     <input type="text"></input>
 
-    <label>Image URL</label>
+    <label>🌄Image URL</label>
     <input type="text"></input>
 
     <Button>Add</Button>
@@ -85,16 +92,16 @@ function FormSplitBill() {
   return <form className="form-split-bill">
     <h2>Split a bill with X</h2>
 
-    <label>Bill value</label>
+    <label>💰Bill value</label>
     <input type="text"></input>
 
-    <label>Your expense</label>
+    <label>🧍Your expense</label>
     <input type="text"></input>
 
-    <label>X's expense</label>
+    <label>👫X's expense</label>
     <input type="text" disabled></input>
 
-    <label>Who is paying the bill</label>
+    <label>🤑Who is paying the bill</label>
     <select>
       <option value="user">You</option>
       <option value="friend">X</option>

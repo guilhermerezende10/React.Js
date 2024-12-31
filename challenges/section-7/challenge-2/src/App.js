0ce4d413b55b0dbdc053c780f0d99d6eaa1a49a3
guilhermerezende10 <rezendeguilherme733@gmail.com>
2024-12-31 09:25:51 -0300

    CHALLENGE #3: Tip Calculator

--- challenges/section-7/challenge-2/src/App.js
+++ challenges/section-7/challenge-2/src/App.js
@@ -1,43 +1,64 @@
 import { useState } from "react";
 
 export default function App() {
-  const [bill, setBill] = useState("");
-  const [percentage1, setPercentage1] = useState(null);
-  const [percentage2, setPercentage2] = useState(null);
+  return (
+    <div>
+      <TipCalculator />
+    </div>
+  );
+}
 
-  let tip = (percentage1 + percentage2) / 2;
+function TipCalculator() {
+  const [bill, setBill] = useState("");
+  const [percentage1, setPercentage1] = useState(0);
+  const [percentage2, setPercentage2] = useState(0);
+
+  function handleReset() {
+    setBill("");
+    setPercentage1(0);
+    setPercentage2(0);
+  }
 
   return (
     <div>
-      <BillInput bill={bill} setBill={setBill}/>
+      <BillInput bill={bill} setBill={setBill} />
       <SelectPercentage value={percentage1} setValue={setPercentage1}>
         <>How did you like the service?</>
       </SelectPercentage>
       <SelectPercentage value={percentage2} setValue={setPercentage2}>
         <>How did your friend like the service?</>
       </SelectPercentage>
-      <DisplayText bill={bill} setBill={setBill} tip={tip}/>
-      <ResetButton setBill={setBill} setPercentage1={setPercentage1} setPercentage2={setPercentage2} /> 
+      <Output bill={bill} percentage1={percentage1} percentage2={percentage2} />
+      <Reset
+        onReset={handleReset}
+      />
     </div>
   );
 }
 
-function BillInput({bill, setBill}) {
+function BillInput({ bill, setBill }) {
   return (
     <div className="form">
-      <>
-        How much was the bill?{" "}
-        <input type="number" className="inputText" value={bill} onChange={(e) => setBill(+e.target.value)}></input>{" "}
-      </>
+      <label>How much was the bill? </label>
+      <input
+        type="number"
+        className="inputText"
+        value={bill}
+        onChange={(e) => setBill(+e.target.value)}
+      ></input>{" "}
     </div>
   );
 }
 
-function SelectPercentage({ children, value, setValue}) {
+function SelectPercentage({ children, value, setValue }) {
   return (
     <div className="form">
       {children}{" "}
-      <select className="inputText" value={value === null ? 5 : value} onChange={(e) => setValue(+e.target.value)}>
+      <select
+        className="inputText"
+        value={value}
+        onChange={(e) => setValue(+e.target.value)}
+      >
         <option value="0">Dissatisfied (0%)</option>
         <option value="5">It was ok (5%)</option>
         <option value="10">It was good (10%)</option>
@@ -47,22 +68,23 @@ function SelectPercentage({ children, value, setValue}) {
   );
 }
 
-function DisplayText({bill, tip}) {
+function Output({ bill, percentage1, percentage2 }) {
+  let tip = (percentage1 + percentage2) / 2;
   return (
     <div className="form">
-      <h3>{`You pay $${+bill + tip} ($${bill === "" ? 0 : bill} + $${tip} tip)`}</h3>
+      <h3>{`You pay $${+bill + tip} ($${
+        bill === "" ? 0 : bill
+      } + $${tip} tip)`}</h3>
     </div>
   );
 }
 
-function ResetButton({setBill, setPercentage1, setPercentage2}) {
-  function onClear() {
-    setBill("")
-    setPercentage1(null)
-    setPercentage2(null)
-  }
-  return <div>
-    <button className="inputButton" onClick={() => onClear()}>Reset</button>
-  </div>
+function Reset({ setBill, setPercentage1, setPercentage2, onReset }) {
+  return (
+    <div>
+      <button className="inputButton" onClick={() => onReset()}>
+        Reset
+      </button>
+    </div>
+  );
 }
-

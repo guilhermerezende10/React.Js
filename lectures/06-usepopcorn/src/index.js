fbc31101f7f0fd746b84f8921dc2182a8ee963a5
guilhermerezende10 <rezendeguilherme733@gmail.com>
2025-01-03 15:52:47 -0300

    Improving Reusability With Props

--- lectures/06-usepopcorn/src/index.js
+++ lectures/06-usepopcorn/src/index.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 // import './index.css';
 // import App from './App';
-import StarRating from "./StarRating"
+import StarRating from "./StarRating";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5}/>
+    <StarRating maxRating={5} />
+    <StarRating
+      size={50}
+      color={"blue"}
+      defaultRating={3}
+      messages={[
+        "uma merda",
+        "cocô",
+        "ok",
+        "maneirin",
+        "tamo falando da elite",
+      ]} 
+    />
   </React.StrictMode>
 );
-

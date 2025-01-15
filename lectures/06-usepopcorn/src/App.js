df8c23da6f90a50d131a3d95649b789852a89241
guilhermerezende10 <rezendeguilherme733@gmail.com>
2025-01-15 10:48:48 -0300

    Refs to Select DOM Elements

--- lectures/06-usepopcorn/src/App.js
+++ lectures/06-usepopcorn/src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import StarRating from "./StarRating";
 
@@ -156,11 +156,23 @@ function Logo() {
 }
 
 function Search({ query, setQuery }) {
-  useEffect(function(){
-    const el = document.querySelector('.search')
-    console.log(el)
-    el.focus()
-  },[])
+  const inputEl = useRef(null);
+
+  useEffect(function () {
+
+    function callback(e) {
+      if(document.activeElement === inputEl.current) return; 
+
+      if(e.code === 'Enter') {
+        inputEl.current.focus();
+        setQuery("")
+      }
+    }
+
+    document.addEventListener('keydown', callback)
+    return () => document.addEventListener('keydown', callback)
+
+  }, []);
 
   return (
     <input
@@ -169,6 +181,7 @@ function Search({ query, setQuery }) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
     />
   );
 }

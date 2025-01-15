5289208864dde99592806a8d049aaf315352e1d1
guilhermerezende10 <rezendeguilherme733@gmail.com>
2025-01-15 10:49:36 -0300

    Refs to Select DOM Elements

--- lectures/06-usepopcorn/src/App.js
+++ lectures/06-usepopcorn/src/App.js
@@ -172,7 +172,7 @@ function Search({ query, setQuery }) {
     document.addEventListener('keydown', callback)
     return () => document.addEventListener('keydown', callback)
 
-  }, []);
+  }, [setQuery]);
 
   return (
     <input

29185b04e14693a1a56c8cf8f4641a6a81c8590d
guilhermerezende10 <rezendeguilherme733@gmail.com>
2025-01-21 20:21:09 -0300

    Handling Loading, Error, and Ready Status

--- lectures/08-react-quiz/src/App.js
+++ lectures/08-react-quiz/src/App.js
@@ -1,6 +1,9 @@
 import Header from "./Header";
+import Loader from "./Loader";
 import Main from "./Main";
+import Error from "./Error";
 import { useEffect, useReducer } from "react";
+import StartScreen from "./startScreen";
 
 const initialState = {
   questions: [],
@@ -19,7 +22,10 @@ function reducer(state, action) {
 }
 
 export default function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+
+  const numQuestions = questions.length;
+
   useEffect(function () {
     async function fetchData() {
       try {
@@ -27,7 +33,7 @@ export default function App() {
         const data = await res.json();
         dispatch({ type: "dataReceived", payload: data });
       } catch (err) {
-        dispatch({type: 'dataFailed'})
+        dispatch({ type: "dataFailed" });
       }
     }
     fetchData();
@@ -36,8 +42,9 @@ export default function App() {
     <div className="app">
       <Header />
       <Main>
-        <p>1/15</p>
-        <p>Question?</p>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && <StartScreen numQuestions={numQuestions}/>}
       </Main>
     </div>
   );

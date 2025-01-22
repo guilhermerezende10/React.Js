a8454c5c53acdb8e94236deb041b63a7302498b2
guilhermerezende10 <rezendeguilherme733@gmail.com>
2025-01-22 08:50:18 -0300

    Finishing a Quiz

--- lectures/08-react-quiz/src/components/App.js
+++ lectures/08-react-quiz/src/components/App.js
@@ -7,6 +7,7 @@ import StartScreen from "./startScreen";
 import Question from "./Question";
 import NextButton from "./NextButton";
 import Progress from "./Progress";
+import FinishScreen from "./FinishScreen";
 
 const initialState = {
   questions: [],
@@ -14,6 +15,7 @@ const initialState = {
   index: 0,
   answer: null,
   points: 0,
+  highscore: 0,
 };
 
 function reducer(state, action) {
@@ -35,21 +37,31 @@ function reducer(state, action) {
             : state.points,
       };
 
-    case 'nextQuestion': 
-    return {...state, index: state.index + 1, answer: null}
+    case "nextQuestion":
+      return { ...state, index: state.index + 1, answer: null };
+    case "finish":
+      return {
+        ...state,
+        status: "finished",
+        highscore:
+          state.points > state.highscore ? state.points : state.highscore,
+      };
     default:
       throw new Error("Action unknown");
   }
 }
 
 export default function App() {
-  const [{ questions, status, index, answer, points }, dispatch] = useReducer(
+  const [{ questions, status, index, answer, points, highscore }, dispatch] = useReducer(
     reducer,
     initialState
   );
 
   const numQuestions = questions.length;
-  const maxPossiblePoints = questions.reduce((prev, cur) => prev + cur.points, 0)
+  const maxPossiblePoints = questions.reduce(
+    (prev, cur) => prev + cur.points,
+    0
+  );
 
   useEffect(function () {
     async function fetchData() {
@@ -74,14 +86,35 @@ export default function App() {
         )}
         {status === "active" && (
           <>
-          <Progress index={index} numQuestions={numQuestions} points={points} maxPossiblePoints={maxPossiblePoints} answer={answer}/>
-          <Question
-            question={questions[index]}
-            dispatch={dispatch}
-            answer={answer}
-          />
-          <NextButton dispatch={dispatch} answer={answer}/> 
+            <Progress
+              index={index}
+              numQuestions={numQuestions}
+              points={points}
+              maxPossiblePoints={maxPossiblePoints}
+              answer={answer}
+            />
+            <Question
+              question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+            />
+            <NextButton
+              dispatch={dispatch}
+              answer={answer}
+              index={index}
+              numQuestions={numQuestions}
+            />
           </>
+        )}
+
+        {status === "finished" && (
+          <FinishScreen
+            points={points}
+            maxPossiblePoints={maxPossiblePoints}
+            index={index}
+            numQuestions={numQuestions}
+            highscore={highscore}
+          />
         )}
       </Main>
     </div>
--- lectures/08-react-quiz/src/components/NextButton.js
+++ lectures/08-react-quiz/src/components/NextButton.js
@@ -1,12 +1,22 @@
-function NextButton({ dispatch, answer }) {
+function NextButton({ dispatch, answer, index, numQuestions }) {
   if (answer === null) return null;
 
-  return (
+  if(index < numQuestions - 1) return (
     <button
       className="btn btn-ui"
       onClick={() => dispatch({ type: "nextQuestion" })}
     >
       Next
+    </button>
+  );
+
+  console.log(index, numQuestions - 1)
+  if(index === numQuestions - 1) return (
+    <button
+      className="btn btn-ui"
+      onClick={() => dispatch({ type: "finish" })}
+    >
+      Finish
     </button>
   );
 }

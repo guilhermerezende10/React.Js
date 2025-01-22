d1865f1147429a83c01a913528f3462da18cfd3c
guilhermerezende10 <rezendeguilherme733@gmail.com>
2025-01-22 15:44:16 -0300

    Linking Between Routes With <Link /> and <NavLink />

---
+++ lectures/09-worldwise/src/pages/Pagenotfound.jsx
@@ -0,0 +1,12 @@
+import PageNav from "../components/PageNav";
+
+function Pagenotfound() {
+  return (
+    <div>
+      <PageNav />
+      Not found
+    </div>
+  );
+}
+
+export default Pagenotfound;

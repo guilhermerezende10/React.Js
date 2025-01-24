770120557f44b3778a75679ea18fce3a0a87359b
guilhermerezende10 <rezendeguilherme733@gmail.com>
2025-01-24 09:27:03 -0300

    Programmatic Navigation with useNavigate

--- lectures/09-worldwise/src/components/CountryList.jsx
+++ lectures/09-worldwise/src/components/CountryList.jsx
@@ -21,8 +21,8 @@ function CountriesList({ cities, isLoading }) {
 
   return (
     <ul className={styles.countryList}>
-      {countries?.map((country, i) => (
-        <CountryItem country={country} key={cities[i].id} />
+      {countries?.map((country) => (
+        <CountryItem country={country} key={country.country} />
       ))}
     </ul>
   );

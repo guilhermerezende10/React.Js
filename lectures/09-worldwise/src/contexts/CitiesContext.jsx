0b2a6d7450e85cb455e9f3c39b8f65f7a8f186d8
guilhermerezende10 <rezendeguilherme733@gmail.com>
2025-01-24 12:03:01 -0300

    Back to "WorldWise": Creating a CitiesContext

---
+++ lectures/09-worldwise/src/contexts/CitiesContext.jsx
@@ -0,0 +1,38 @@
+import { createContext, useEffect, useState } from "react";
+
+const CitiesContext = createContext();
+
+const BASE_URL = "http://localhost:8000/cities";
+
+function CitiesProvider({ children }) {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(BASE_URL);
+        const data = await res.json();
+        setCities(data);
+      } catch (err) {
+        console.error(err);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+  return (
+    <CitiesContext.Provider
+      value={{
+        cities,
+        isLoading,
+      }}
+    >
+      {children}
+    </CitiesContext.Provider>
+  );
+}
+
+export { CitiesProvider };

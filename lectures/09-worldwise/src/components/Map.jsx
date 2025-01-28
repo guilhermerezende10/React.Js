9820abf98033bb15b378a7a805fdf134b558c29a
guilhermerezende10 <rezendeguilherme733@gmail.com>
2025-01-28 12:10:45 -0300

    Displaying City Markers on Map

--- lectures/09-worldwise/src/components/Map.jsx
+++ lectures/09-worldwise/src/components/Map.jsx
@@ -2,15 +2,18 @@ import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 import { TileLayer, Marker, Popup, MapContainer } from "react-leaflet";
 import { useState } from "react";
+import { useCities } from "../contexts/CitiesContext";
 
 function Map() {
   const navigate = useNavigate();
+  const { cities } = useCities();
   const [mapPosition, setMapPosition] = useState([40, 0]);
   // useNavigate returns a function that moves to any url
 
   const [searchParams, setSearchParams] = useSearchParams();
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
+
   return (
     <div className={styles.mapContainer}>
       <MapContainer
@@ -23,11 +26,16 @@ function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
-        <Marker position={mapPosition}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              <span>{city.emoji}</span> <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
       </MapContainer>
     </div>
   );

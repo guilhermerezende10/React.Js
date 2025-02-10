f8ad870bc91dcc5aa259572fc6d8d8fb93e000a0
guilhermerezende10 <rezendeguilherme733@gmail.com>
2025-02-10 17:55:54 -0300

    Update App.jsx

--- lectures/09-worldwise/src/App.jsx
+++ lectures/09-worldwise/src/App.jsx
@@ -1,4 +1,9 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+
+import { CitiesProvider } from "./contexts/CitiesContext";
+import { AuthProvider } from "./contexts/FakeAuthContext";
+import ProtectedRoute from "./pages/ProtectedRoute";
+
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
@@ -9,9 +14,6 @@ import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
-import { CitiesProvider } from "./contexts/CitiesContext";
-import { AuthProvider } from "./contexts/FakeAuthContext";
-import ProtectedRoute from "./pages/ProtectedRoute";
 
 function App() {
   return (

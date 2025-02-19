de09c5cf5c76767ed8aae7e502a83e3df0c9885d
guilhermerezende10 <rezendeguilherme733@gmail.com>
2025-02-19 06:31:07 -0300

    Fetching Orders

--- lectures/13-fast-react-pizza/src/ui/Header.jsx
+++ lectures/13-fast-react-pizza/src/ui/Header.jsx
@@ -1,9 +1,12 @@
 import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/searchOrder";
 
 function Header() {
   return (
     <header>
       <Link to="/">Fast React Pizza Co.</Link>
+
+      <SearchOrder />
       <p>Guilherme</p>
     </header>
   );

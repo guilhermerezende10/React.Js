612f2451365fe1e214c6209084131875511ee230
Guilherme Rezende Silva <101947315+guilhermerezende10@users.noreply.github.com>
2025-09-08 22:10:41 -0300

    Building a Reusable Context Menu

--- lectures/14-the-wild-oasis/src/features/cabins/CabinTable.jsx
+++ lectures/14-the-wild-oasis/src/features/cabins/CabinTable.jsx
@@ -4,7 +4,7 @@ import Spinner from "../../ui/Spinner";
 import CabinRow from "./CabinRow";
 import Table from "../../ui/Table";
 import { useCabins } from "./useCabins";
-
+import Menus from "../../ui/Menus";
 
 const TableHeader = styled.header`
   display: grid;
@@ -22,25 +22,27 @@ const TableHeader = styled.header`
 `;
 
 function CabinTable() {
-  const {isLoading, cabins} = useCabins()
+  const { isLoading, cabins } = useCabins();
 
   if (isLoading) return <Spinner />;
 
   return (
-    <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
-      <Table.Header role="row">
-        <div></div>
-        <div>Cabin</div>
-        <div>Capacity</div>
-        <div>Price</div>
-        <div>Discount</div>
-        <div></div>
-      </Table.Header>
-      <Table.Body data={cabins} render={(cabin) => (
-        <CabinRow cabin={cabin} key={cabin.id} />
-      )}/>
-
-    </Table>
+    <Menus>
+      <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
+        <Table.Header role="row">
+          <div></div>
+          <div>Cabin</div>
+          <div>Capacity</div>
+          <div>Price</div>
+          <div>Discount</div>
+          <div></div>
+        </Table.Header>
+        <Table.Body
+          data={cabins}
+          render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
+        />
+      </Table>
+    </Menus>
   );
 }
 

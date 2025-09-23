57878ec527ced904697fc11ec25f22d3e3e172e7
Guilherme Rezende Silva <101947315+guilhermerezende10@users.noreply.github.com>
2025-09-22 21:30:16 -0300

    API-Side Filtering: Filtering Bookings

--- lectures/14-the-wild-oasis/src/features/bookings/useBookings.js
+++ lectures/14-the-wild-oasis/src/features/bookings/useBookings.js
@@ -1,14 +1,19 @@
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
 
 export function useBookings() {
+  const [searchParams] = useSearchParams();
+  const filteredValue = searchParams.get("status");
+  const filter = !filteredValue || filteredValue === "all" ? null : {field: "status", value: filteredValue, method: "eq"};
+
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ["bookings"],
-    queryFn: getBookings,
+    queryKey: ["bookings", filter],
+    queryFn: () => getBookings({filter}),
   });
 
   return { isLoading, bookings, error };

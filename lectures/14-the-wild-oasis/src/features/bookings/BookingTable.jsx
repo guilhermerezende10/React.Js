742b0361ebba864680ea0b3f3e611867f1b8c644
Guilherme Rezende Silva <101947315+guilhermerezende10@users.noreply.github.com>
2025-09-23 22:19:22 -0300

    update

--- lectures/14-the-wild-oasis/src/features/bookings/BookingTable.jsx
+++ lectures/14-the-wild-oasis/src/features/bookings/BookingTable.jsx
@@ -4,6 +4,7 @@ import Menus from "../../ui/Menus";
 import Empty from "../../ui/Empty";
 import { useBookings } from "./useBookings";
 import Spinner from "../../ui/Spinner";
+import Pagination from "../../ui/Pagination";
 
 function BookingTable() {
   const {bookings, isLoading} = useBookings();
@@ -31,7 +32,11 @@ function BookingTable() {
             <BookingRow key={booking.id} booking={booking} />
           )}
         />
+      <Table.Footer>
+        <Pagination count={23}/>
+      </Table.Footer>
       </Table>
+
     </Menus>
   );
 }
